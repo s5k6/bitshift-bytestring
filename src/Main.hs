0a4f95464390d02bd32b2edceb2ef461dafa68ed
Stefan Klinger <git@stefan-klinger.de>
2024-07-19 23:17:33 +0200

    shift by more than one byte

--- src/Main.hs
+++ src/Main.hs
@@ -11,7 +11,7 @@ bitsRight :: Int -> B.ByteString -> B.ByteString
 
 bitsRight n
   | 0 < n && n < 8 = B.pack . go 0 . B.unpack
-  | otherwise = error $ "argument out of range 1..7"
+  | otherwise = error "bitsRight: argument out of range 1..7"
   where
     go y (x:xs) = ( shiftL y (8-n) .|. shiftR x n ) : go x xs
     go _ _ = []
@@ -22,7 +22,7 @@ bitsLeft :: Int -> B.ByteString -> B.ByteString
 
 bitsLeft n
   | 0 < n && n < 8 = B.pack . start . B.unpack
-  | otherwise = error $ "argument out of range 1..7"
+  | otherwise = error "bitsLeft: argument out of range 1..7"
   where
     start (x:xs) = go x xs
     start [] = []
@@ -33,13 +33,38 @@ bitsLeft n
 
 bytesRight :: Int -> B.ByteString -> B.ByteString
 
-bytesRight n bs = B.replicate n 0 <> B.dropEnd n bs
+bytesRight n bs
+  | n < 1 = error "bytesRight: negative argument"
+  | n < B.length bs = B.replicate n 0 <> B.dropEnd n bs
+  | otherwise = B.replicate (B.length bs) 0
 
 
 
 bytesLeft :: Int -> B.ByteString -> B.ByteString
 
-bytesLeft n bs = B.drop n bs <> B.replicate n 0
+bytesLeft n bs
+  | n < 1 = error "bytesLeft: negative argument"
+  | n < B.length bs = B.drop n bs <> B.replicate n 0
+  | otherwise = B.replicate (B.length bs) 0
+
+
+
+shift :: Int -> B.ByteString -> B.ByteString
+
+shift n | n > 0 = case n `divMod` 8 of
+                    (by,0) -> bytesRight by
+                    (0,bi) -> bitsRight bi
+                    (by,bi) -> bitsRight bi . bytesRight by
+
+shift n | n < 0 = case negate n `divMod` 8 of
+                    (by,0) -> bytesLeft by
+                    (0,bi) -> bitsLeft bi
+                    (by,bi) -> bitsLeft bi . bytesLeft by
+
+shift 0 = id
+
+
+
 
 
 display :: B.ByteString -> IO ()
@@ -51,18 +76,9 @@ display =
 
 t0 = B.pack [8]
 
-t1 = B.pack [255, 0, 183 , 0, 255]
+t1 = B.pack [255, 1, 128, 255]
 
 main :: IO ()
 
 main = do
-  display t0
-  display $ bitsRight 1 t0
-  putStrLn ""
-
-  display t1
-  mapM_ display [ bitsRight n t1 | n <- [1 .. 7] ]
-  putStrLn ""
-
-  display t1
-  mapM_ display [ bitsLeft n t1 | n <- [1 .. 7] ]
+  mapM_ display [ shift n t1 | n <- [-33 .. 33] ]
