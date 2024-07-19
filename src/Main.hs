32988b51a383fec9fcefcecea01e1e0b8e9ae44d
Stefan Klinger <git@stefan-klinger.de>
2024-07-19 23:59:46 +0200

    refactored error-checking

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,4 @@
-module Main ( main ) where
+module Main ( main, bitsRight, bitsLeft, bytesRight, bytesLeft, shift ) where
 
 import qualified Data.ByteString as B
 import Data.Bits ( shiftL, shiftR, (.|.), testBit )
@@ -8,8 +8,12 @@ import Data.Bits ( shiftL, shiftR, (.|.), testBit )
 bitsRight :: Int -> B.ByteString -> B.ByteString
 
 bitsRight n
-  | 0 < n && n < 8 = B.pack . go 0
+  | 0 < n && n < 8 = bitsRight_ n
   | otherwise = error "bitsRight: argument out of range 1..7"
+
+bitsRight_ :: Int -> B.ByteString -> B.ByteString
+
+bitsRight_ n = B.pack . go 0
   where
     go y bs = case B.uncons bs of
       Just (x, xs) -> ( shiftL y (8-n) .|. shiftR x n ) : go x xs
@@ -20,8 +24,12 @@ bitsRight n
 bitsLeft :: Int -> B.ByteString -> B.ByteString
 
 bitsLeft n
-  | 0 < n && n < 8 = B.pack . maybe [] (uncurry go) . B.uncons
+  | 0 < n && n < 8 = bitsLeft_ n
   | otherwise = error "bitsLeft: argument out of range 1..7"
+
+bitsLeft_ :: Int -> B.ByteString -> B.ByteString
+
+bitsLeft_ n = B.pack . maybe [] (uncurry go) . B.uncons
   where
     go y bs = case B.uncons bs of
       Just (x, xs) -> ( shiftL y n .|. shiftR x (8-n) ) : go x xs
@@ -31,8 +39,13 @@ bitsLeft n
 
 bytesRight :: Int -> B.ByteString -> B.ByteString
 
-bytesRight m bs
+bytesRight m
   | m < 1 = error "bytesRight: negative argument"
+  | otherwise = bytesRight_ m
+
+bytesRight_ :: Int -> B.ByteString -> B.ByteString
+
+bytesRight_ m bs
   | m < B.length bs = B.replicate m 0 <> B.dropEnd m bs
   | otherwise = B.replicate (B.length bs) 0
 
@@ -40,8 +53,13 @@ bytesRight m bs
 
 bytesLeft :: Int -> B.ByteString -> B.ByteString
 
-bytesLeft m bs
+bytesLeft m
   | m < 1 = error "bytesLeft: negative argument"
+  | otherwise = bytesLeft_ m
+
+bytesLeft_ :: Int -> B.ByteString -> B.ByteString
+
+bytesLeft_ m bs
   | m < B.length bs = B.drop m bs <> B.replicate m 0
   | otherwise = B.replicate (B.length bs) 0
 
@@ -49,18 +67,21 @@ bytesLeft m bs
 
 shift :: Int -> B.ByteString -> B.ByteString
 
-shift d | d < 0 = f (negate d) bitsLeft bytesLeft
-        | d > 0 = f d bitsRight bytesRight
-        | otherwise = id
+shift d
+  | d < 0 = f (negate d) bitsLeft_ bytesLeft_
+  | d > 0 = f d bitsRight_ bytesRight_
+  | otherwise = id
   where
-    f s bits bytes = case s `divMod` 8 of
-                       (m,0) -> bytes m
-                       (0,n) -> bits n
-                       (m,n) -> bits n . bytes m
+    f s bits bytes =
+      case s `divMod` 8 of
+        (m,0) -> bytes m
+        (0,n) -> bits n
+        (m,n) -> bits n . bytes m
 
-
-
-
+    {- Note: This could be further optimised.  E.g., it is not necessary to
+    bit-shift the entire string when combined with byte-shifting.  It
+    would be sufficient to bit-shift the part of the original string
+    that survives the shift, and then pad with zeros .-}
 
 
 display :: B.ByteString -> IO ()
@@ -70,9 +91,10 @@ display =
     f b = showString [ if testBit b i then '1' else '0' | i <- [7, 6 .. 0] ]
 
 
-t0 = B.pack [8]
-
+t1 :: B.ByteString
 t1 = B.pack [255, 1, 128, 255]
+
+
 
 main :: IO ()
 
